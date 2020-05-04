17bdd2041716f766c7f536f9d3e958eb017dc659
DiFine <biebel2003@gmail.com>
2020-05-04 16:32:29 +0400

    Add CSS minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
+const cssFiles = ('./css/*.css')
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename')
+
 
 gulp.task('browser-sync', function() {
   browserSync.init({
@@ -8,4 +12,15 @@ gulp.task('browser-sync', function() {
       }
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('styles', function () {
+
+  return gulp.src(cssFiles)
+  .pipe(cleanCSS({level: 2}))
+  .pipe(rename({
+    suffix: '.min'
+  }))
+  .pipe(gulp.dest('./style'))
+
 });
